33386ab4de5ed1d2c8d949650f4793968fe74657
amaran-th <songsy405@naver.com>
2025-11-13 21:21:21 +0900

    design: 로그인/회원가입 페이지의 컨텐츠 최소 높이 값 조정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,8 @@
 import LoginForm from "@/features/auth/components/LoginForm";
-import { HEADER_HEIGHT } from "@/lib/data";
 
 export default function LoginPage() {
   return (
-    <div
-      className="grid lg:grid-cols-2"
-      style={{ minHeight: `calc(100svh - ${HEADER_HEIGHT}px)` }}
-    >
+    <div className="grid lg:grid-cols-2 grow">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start"></div>
         <div className="flex flex-1 items-center justify-center">
