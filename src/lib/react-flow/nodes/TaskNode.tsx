55ea5ce722e95f8b0f979921d843da84543006e3
amaran-th <songsy405@naver.com>
2026-01-07 23:22:47 +0900

    feat: 카테고리 Select 기본 value 설정
    #44

--- src/lib/react-flow/nodes/TaskNode.tsx
+++ src/lib/react-flow/nodes/TaskNode.tsx
@@ -105,7 +105,9 @@ function CategorySprintSelector({ task }: { task: TaskWithRelations }) {
           }}
         >
           <SelectTrigger className="max-h-fit w-full">
-            <SelectValue placeholder="카테고리 선택" />
+            <SelectValue placeholder="카테고리 선택">
+              {task.category ? task.category.name : ""}
+            </SelectValue>
           </SelectTrigger>
           <SelectContent>
             <div className="flex justify-between">
